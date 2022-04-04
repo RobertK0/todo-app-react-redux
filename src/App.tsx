5b36151bc09ed2ccb32fa64785a7436d8593cdc8
Zero0k <robykaurin@gmail.com>
2022-04-05 01:41:42 +0200

    Added initial functionality
    Added rendering of individual tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import styles from "./App.module.css";
 import Header from "./components/Header";
+import InputForm from "./components/InputForm";
+import TaskContainer from "./components/Tasks/TaskContainer";
+import Card from "./components/UI/Card";
 
 function App() {
   return (
     <main className={styles.app}>
       <Header />
+      <Card>
+        <InputForm />
+      </Card>
+      <Card>
+        <TaskContainer />
+      </Card>
     </main>
   );
 }
